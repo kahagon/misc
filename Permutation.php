792a6bcdc2fb2c2b1bf7f5438997d833db28c511
oasynnoum <oasynnoum@UbuntuPS.(none)>
2013-03-30 03:06:17 +0900

    fix a bug

--- Permutation.php
+++ Permutation.php
@@ -10,10 +10,11 @@ class Permutation implements Iterator {
 
     public function __construct(array $list) {
         $this->depth = count($list);
-        foreach ($list as $l) {
+        for ($i = 0; $i < $this->depth; $i++) {
+            $l = $list[$i];
             $this->elements[] = (object)array(
                 'head' => $l,
-                'tail' => new Permutation(array_diff($list, array($l)))); 
+                'tail' => new Permutation($this->except($list, $i))); 
         } 
     }
 
@@ -24,6 +25,16 @@ class Permutation implements Iterator {
         $currentTail = $this->elements[$this->currentElementIndex]->tail->current();
         return array_merge(array($currentHead), !$currentTail ? array() : $currentTail);
     } 
+
+    public function except(array $list, $exceptIndex) {
+        $shifted = array();
+        $length = count($list);
+        for ($i = 0; $i < $length; $i++) {
+            if ($i == $exceptIndex) continue;
+            $shifted[] = $list[$i];
+        }
+        return $shifted;
+    }
 
     public function next() {
         $this->_next();
@@ -64,12 +75,12 @@ class Permutation implements Iterator {
         return $this->currentElementIndex < count($this->elements);
     }
 }
-/*
-$permutation = new Permutation(array('a', 'b', 'c', 'd', 'e'));
+//*
+$permutation = new Permutation(str_split('hello'));
 foreach ($permutation as $p) {
         $str = '';
         foreach ($p as $e) {
-            $str .= $e . ' ';
+            $str .= $e . '';
         }
         print $str . PHP_EOL;
 }
